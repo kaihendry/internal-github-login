b1263b37f1261d56fc87c55f8dc9161aa037ee25
Kai Hendry <hendry@webconverger.com>
2019-05-19 09:02:04 +0800

    Simple integration test

--- main_test.go
+++ main_test.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
-
-	"github.com/appleboy/gofight"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestRoutes(t *testing.T) {
-	r := gofight.New()
-	r.GET("/admin").
-		Run(BasicEngine(), func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
-			assert.Contains(t, r.Body.String(), "/google/login")
-		})
-	r.GET("/").
-		Run(BasicEngine(), func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
-			assert.Contains(t, r.Body.String(), "Admin page")
-		})
+func TestCount(t *testing.T) {
 
+	tests := []struct {
+		name string
+		uri  string
+		want string
+	}{
+		{"Admin", "/admin", "Sign in - Google Accounts"},
+	}
+
+	ts := httptest.NewServer(BasicEngine())
+	defer ts.Close()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			url := ts.URL + tt.uri
+			resp, _ := http.Get(url)
+			if resp.StatusCode != http.StatusOK {
+				t.Fatalf("got %d, want %d", resp.StatusCode, http.StatusOK)
+			}
+			respBody, _ := ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
+			got := string(respBody)
+			if !strings.Contains(got, tt.want) {
+				t.Errorf("got %s, Want %s", got, tt.want)
+			}
+		})
+	}
 }
